046b599420ee43fa7d1e68459daca81ae7e7199e
Andrii Rymaruk <155837728+arymcourse@users.noreply.github.com>
2024-02-11 20:24:23 +0200

    Update task1.js

--- src/HW2/task1.js
+++ src/HW2/task1.js
@@ -10,6 +10,18 @@ LocalStorageManager.prototype.setItem = function(key, value) {
 };
 
 LocalStorageManager.prototype.getItem = function(key) {
+  /*
+    Якщо покривати додатковою перевіркою localStorage, то краще покрити через try/catch. 
+    Тоді у випадку коли localStorage буде false (що дуже мало йммовірно)) або ж помилка із JSON.parse то у нас відпрацює блок catch
+    try {
+      const value = JSON.parse(localStorage.getItem(key));
+      console.log(`Data with key '${key}' successfully retrieved from localStorage.`);
+      return value;
+    } catch (error) {
+      console.error('LocalStorage is not supported in this environment.');
+      return null;
+    }
+  */
   if (localStorage) {
     const value = JSON.parse(localStorage.getItem(key));
     console.log(`Data with key '${key}' successfully retrieved from localStorage.`);
